8f9e855bf0b077bfc7211ee93b69113f5a662a42
Kevin <kevin3.blondel@epitech.eu>
2016-01-28 17:20:49 +0100

    exercice 4

--- ex_2.php
+++ ex_2.php
@@ -4,8 +4,8 @@ function tri_fusion ( $tab , &$ord ) {
 
     else {
 
-        $tab1 = [];
-        $tab2 = [];
+        $left = [];
+        $right = [];
 
         for( $i = 0; $i < count( $tab ); $i++) {
             if( $i < ( count( $tab ) ) / 2 )
@@ -14,37 +14,13 @@ function tri_fusion ( $tab , &$ord ) {
                 $tab2[] = $tab[ $i ];
         }
 
-        var_dump($tab1,$tab2)
 
-        tri_fusion( $tab1, $ord);
-        tri_fusion( $tab2, $ord);
-
-        $i = 0;
-        $i1 = $i2 = 0;
-
-        while( $i1 < count( $tab1 ) && $i2 < count( $tab2 ) ) {
-            if( $tab1[ $i1 ] < $tab2[ $i2 ] )
-                $tab[ $i ] = $tab1[ $i1++ ];
-            else
-                $tab[ $i ] = $tab2[ $i2++ ];
-            $i++;
-        }
-
-        while( $i1 < count( $tab1 ) ) {
-            $tab[ $i ] = $tab1[ $i1++ ];
-            $i++;
-        }
-        while( $i2 < count( $tab2 ) ) {
-            $tab[ $i ] = $tab2[ $i2++ ];
-            $i++;
-        }
-        $ord = $tab;
-    }
 }
 
 function fusioner(){
 
 }
+
 $test = [1,3,2,8,9,6,5];
 $test1 = [];
 tri_fusion($test, $test1);
--- ex_4.php
+++ ex_4.php
@@ -1,1 +1,13 @@
 <?php
+function decompress_str($string_to_decompress){
+    $str = str_split($string_to_decompress);
+    $res = '';
+    for($i = 0 ; $i < count($str) ; $i++){
+        if ($i % 2 == 0 || $i == 0){
+            for($j = 0; $j < $str[$i] ; $j++){
+                $res .= $str[$i+1];
+            }
+        }
+    }
+    return $res;
+}
